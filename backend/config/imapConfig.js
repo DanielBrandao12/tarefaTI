dcfdfac043ea6d2c86d024a798edb67f5ca2c86b
Daniel Rodrigues Brandão <daniel.brandao@fatec.br>
2025-02-07 16:13:48 -0300

    versão 2.1

--- backend/config/imapConfig.js
+++ backend/config/imapConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
     imapConfig: {
       imap: {
-        user: 'servicedesk@fatecbpaulista.edu.br', // E-mail do usuário
+        //user: 'servicedesk@fatecbpaulista.edu.br', // E-mail do usuário
         password: 'ks@4fn.FU-+HjgE', // Senha fornecida
         host: 'email-ssl.com.br', // Servidor de entrada
         port: 993, // Porta IMAP com SSL/TLS
--- backend/database/config/config.js
+++ backend/database/config/config.js
@@ -1,22 +1,22 @@
 module.exports ={
     "development": {
       "username": "root",
-      "password":"ftcbp183",
-      "database": "bd_service_desk_2.0",
+      "password":"",
+      "database": "bd_sistema_chamado_fatec",
       "host": "127.0.0.1",
       "dialect": "mysql"
     },
     "test": {
       "username": "root",
-      "password":"ftcbp183",
-      "database": "bd_service_desk_2.0",
+      "password":"",
+      "database": "bd_sistema_chamado_fatec",
       "host": "127.0.0.1",
       "dialect": "mysql"
     },
     "production": {
       "username": "root",
-      "password":"ftcbp183",
-      "database": "bd_service_desk_2.0",
+      "password":"",
+      "database": "bd_sistema_chamado_fatec",
       "host": "127.0.0.1",
       "dialect": "mysql"
     }
